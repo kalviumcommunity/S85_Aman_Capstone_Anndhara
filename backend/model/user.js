393fd092335fb38762b8ecebc4aae76741917078
AMAN AMAN <amanrohilla520@gmail.com>
2025-04-23 22:16:13 +0530

    Schema

---
+++ backend/model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const userSchema =new mongoose.Schema({
+    user: {
+        type: String,
+        required: true},
+    email: {
+        type: String,
+        required: true},
+    password: {
+        type: String,
+        required: true},
+    photo: {
+        required: String,},
+    role:{
+        type: String,
+        enum: ['farmer', 'buyer'],
+        required: true},
+    phone:{
+        type: String,
+        required: true},
+        profileImage: String,
+},{ timestamps: true });
+module.exports=mongoose.model('User',userSchema);
