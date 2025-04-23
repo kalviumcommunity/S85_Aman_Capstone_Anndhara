421cceb6ec9d317529227569c9a25b7e04f4da0f
AMAN AMAN <amanrohilla520@gmail.com>
2025-04-23 20:15:57 +0530

    DataBase

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+const Url=process.env.DB_URL;
+
+mongoose.connect(Url).then(()=>{
+console.log('MongoDB Connected');
+}).catch((error)=>{
+    console.error('MongoDB Not Connected',error);
+})
+
+module.exports=mongoose;
