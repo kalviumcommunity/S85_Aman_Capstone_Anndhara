6a5bb6497d50e166c98ca7f27a32c907c956fff4
AMAN AMAN <amanrohilla520@gmail.com>
2025-04-23 20:15:57 +0530

    DataBase

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express=require('express')
+require('dotenv').config();
+const db=require('./db.js');
+const port =8592||process.env.PORT;
+
+const app=express();
+app.use(express.json())
+
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+    
+})
