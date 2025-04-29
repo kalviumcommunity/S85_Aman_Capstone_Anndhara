f6a42129e75adcbafadafcc997c53ef8eadc3203
AMAN AMAN <amanrohilla520@gmail.com>
2025-04-30 01:29:34 +0530

    DashBoard-P

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css' // This is where Tailwind is imported (via @tailwind directives)
+import DashBoard from './component/Dashboard'
+
+function App() {
+  return (
+    <>
+     <DashBoard/>
+    </>
+  )
+}
+
+export default App
